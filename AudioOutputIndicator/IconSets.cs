7d62adf99df511de8936ddd94dc3b4f6bd50ff88
michael <michael.dudek@centrum.cz>
2021-11-17 17:43:03 +0100

    - implemented light/dark icons set and user settings

---
+++ AudioOutputIndicator/IconSets.cs
@@ -0,0 +1,7 @@
+enum IconSets
+{
+    Auto,
+    Light,
+    Dark,
+    Original
+}
--- AudioOutputIndicator/Program.cs
+++ AudioOutputIndicator/Program.cs
@@ -1,22 +1,32 @@
 using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Globalization;
 using System.Linq;
 using System.Reflection;
+using System.Threading;
 using System.Windows.Forms;
 using Windows.Devices.Bluetooth;
 using Windows.Devices.Enumeration;
 using Windows.Media.Devices;
+using Microsoft.Win32;
+using RegistryUtils;
 
-[assembly:AssemblyTitle("Audio Output Indicator")]
+[assembly: AssemblyTitle("Audio Output Indicator")]
 namespace AudioOutputIndicator
 {
     static class Program
     {
+        private static IconSets _selectedIconSet;
         /// <summary>
         ///  The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
+            var culture = new CultureInfo("cs");
+            Thread.CurrentThread.CurrentCulture = culture;
+            Thread.CurrentThread.CurrentUICulture = culture;
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
 
@@ -26,18 +36,41 @@ namespace AudioOutputIndicator
 
         public class IndicatorApplicationContext : ApplicationContext
         {
+            const string OsThemeRegistryPath = @"Software\Microsoft\Windows\CurrentVersion\Themes\Personalize";
+            private RegistryMonitor _monitor;
             private readonly NotifyIcon _notifyIcon;
             private string _defaultAudioId;
+            private Dictionary<IconSets, ToolStripMenuItem> _iconSetsMenu = new Dictionary<IconSets, ToolStripMenuItem>();
 
             const string ICON_PROP_NAME = "System.Devices.Icon";
 
             public IndicatorApplicationContext()
             {
+                _monitor = new RegistryMonitor(RegistryHive.CurrentUser, OsThemeRegistryPath);
+                _monitor.RegChanged += OnRegChanged;
+                _monitor.Start();
+
+
                 // Initialize
                 _notifyIcon = new NotifyIcon();
                 _notifyIcon.Visible = true;
                 _notifyIcon.ContextMenuStrip = new ContextMenuStrip();
-                _notifyIcon.ContextMenuStrip.Items.Add("Exit", null, Exit);
+                var iconSetItem = _notifyIcon.ContextMenuStrip.Items.Add(Resources.Texts.IconSet, Resources.Icons.headphones.ToBitmap()) as ToolStripMenuItem;
+                _iconSetsMenu.Add(IconSets.Auto, iconSetItem.DropDownItems.Add(Resources.Texts.Auto, Resources.Icons.auto.ToBitmap(), (sender, args) => SetIconSet(IconSets.Auto)) as ToolStripMenuItem);
+                _iconSetsMenu.Add(IconSets.Dark, iconSetItem.DropDownItems.Add(Resources.Texts.Dark, Resources.Icons.headphones.ToBitmap(), (sender, args) => SetIconSet(IconSets.Dark)) as ToolStripMenuItem);
+                _iconSetsMenu.Add(IconSets.Light, iconSetItem.DropDownItems.Add(Resources.Texts.Light, Resources.Icons.headphones_white.ToBitmap(), (sender, args) => SetIconSet(IconSets.Light)) as ToolStripMenuItem);
+                _iconSetsMenu.Add(IconSets.Original, iconSetItem.DropDownItems.Add(Resources.Texts.Original, Resources.Icons.headphones_orig.ToBitmap(), (sender, args) => SetIconSet(IconSets.Original)) as ToolStripMenuItem);
+                _notifyIcon.ContextMenuStrip.Items.Add(Resources.Texts.About, Resources.Icons.info.ToBitmap(), ShowAbout);
+                _notifyIcon.ContextMenuStrip.Items.Add(Resources.Texts.Exit, Resources.Icons.exit.ToBitmap(), Exit);
+
+                var initialIconSet = IconSets.Auto;
+                // load user settings
+                var savedIconSet = Properties.Settings.Default["IconSet"].ToString();
+                if (!String.IsNullOrEmpty(savedIconSet))
+                {
+                    Enum.TryParse(savedIconSet, out initialIconSet);
+                }
+                SetIconSet(initialIconSet);
 
                 MediaDevice.DefaultAudioRenderDeviceChanged += (sender, args) =>
                 {
@@ -46,39 +79,66 @@ namespace AudioOutputIndicator
                 ShowDefaultDevice();
             }
 
+            private void OnRegChanged(object? sender, EventArgs e)
+            {
+                if (_selectedIconSet == IconSets.Auto)
+                {
+                    ShowDefaultDevice(true);
+                }
+            }
+
+            private void SetIconSet(IconSets iconSet)
+            {
+                _selectedIconSet = iconSet;
+                
+                // save user settings
+                Properties.Settings.Default["IconSet"] = iconSet.ToString();
+                Properties.Settings.Default.Save();
+
+                foreach (var icSet in _iconSetsMenu.Keys)
+                {
+                    _iconSetsMenu[icSet].Checked = icSet == iconSet;
+                }
+
+                ShowDefaultDevice(true);
+            }
+
             void Exit(object sender, EventArgs e)
             {
+                _monitor.Stop();
                 // Hide tray icon, otherwise it will remain shown until user mouses over it
                 _notifyIcon.Visible = false;
                 Application.Exit();
             }
 
-            private async void ShowDefaultDevice()
+            void ShowAbout(object sender, EventArgs e)
+            {
+                MessageBox.Show($"Audio Input Indicator - {GetAppVersion()}", Resources.Texts.About, MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+
+            private async void ShowDefaultDevice(bool updateIconOnly = false)
             {
 
                 ////Paired bluetooth devices
                 //var PairedBluetoothDevices = await DeviceInformation.FindAllAsync(BluetoothDevice.GetDeviceSelectorFromPairingState(true));
                 ////Connected bluetooth devices
                 //var ConnectedBluetoothDevices = await DeviceInformation.FindAllAsync(BluetoothDevice.GetDeviceSelectorFromConnectionStatus(BluetoothConnectionStatus.Connected));
-                
+
                 // audio devices
                 var audioDevices = await DeviceInformation.FindAllAsync(MediaDevice.GetAudioRenderSelector());
                 var defaultAudioId = MediaDevice.GetDefaultAudioRenderId(AudioDeviceRole.Default);
-                if (_defaultAudioId != defaultAudioId)
+                if (_defaultAudioId != defaultAudioId || updateIconOnly)
                 {
                     var defaultAudio = audioDevices.FirstOrDefault(a => a.Id == defaultAudioId);
 
                     if (defaultAudio != null)
                     {
                         var deviceIconString = defaultAudio.Properties[ICON_PROP_NAME].ToString();
-                        var iconParts = deviceIconString.Split(",");
-                        var icon = IconExtractor.Extract(Environment.ExpandEnvironmentVariables(iconParts[0]),
-                            Int32.Parse(iconParts[1]), false);
 
                         _notifyIcon.Text = defaultAudio.Name;
-                        _notifyIcon.Icon = icon;
+                        _notifyIcon.Icon = getDeviceIcon(deviceIconString);
 
-                        if (_defaultAudioId != null)
+                        if (_defaultAudioId != null && !updateIconOnly)
                         {
                             _notifyIcon.BalloonTipTitle = @"Default audio device changed:";
                             _notifyIcon.BalloonTipText = defaultAudio.Name;
@@ -92,6 +152,60 @@ namespace AudioOutputIndicator
                     }
                 }
                 _defaultAudioId = defaultAudioId;
+            }
+
+            private Icon getDeviceIcon(string deviceIconString)
+            {
+                // %windir%\system32\mmres.dll,-3010
+                // "C:\\Windows\\System32\\DDORes.dll,-2033"
+
+                if (_selectedIconSet != IconSets.Original)
+                {
+                    var iconSetToUse = _selectedIconSet;
+                    if (iconSetToUse == IconSets.Auto)
+                    {
+                        // set default value
+                        iconSetToUse = IconSets.Original;
+                        // use icons set by OS theme
+                        using (var key = Registry.CurrentUser.OpenSubKey($"{OsThemeRegistryPath}"))
+                        {
+                            if (key != null) {
+                                var value = key.GetValue("AppsUseLightTheme") as int?;
+                                iconSetToUse = value == 1 ? IconSets.Dark : IconSets.Light;
+                            }
+                        }
+                    }
+                    if (deviceIconString.ToLower().IndexOf("mmres.dll", StringComparison.Ordinal) != -1)
+                    {
+                        var iconNo = deviceIconString.Substring(deviceIconString.IndexOf(",", StringComparison.Ordinal) + 1);
+                        switch (iconNo)
+                        {
+                            case "-3010":
+                                return iconSetToUse == IconSets.Light ? Resources.Icons.speakers_white : Resources.Icons.speakers;
+                        }
+                    }
+                    if (deviceIconString.ToLower().IndexOf("ddores.dll", StringComparison.Ordinal) != -1)
+                    {
+                        var iconNo = deviceIconString.Substring(deviceIconString.IndexOf(",", StringComparison.Ordinal) + 1);
+                        switch (iconNo)
+                        {
+                            case "-2033":
+                                return iconSetToUse == IconSets.Light ? Resources.Icons.headphones_white : Resources.Icons.headphones;
+                        }
+                    }
+                }
+
+                // show original device icon
+                var iconParts = deviceIconString.Split(",");
+                var icon = IconExtractor.Extract(Environment.ExpandEnvironmentVariables(iconParts[0]),
+                    int.Parse(iconParts[1]), false);
+                return icon;
+
+            }
+
+            private string GetAppVersion()
+            {
+                return GetType().Assembly.GetName().Version.ToString();
             }
 
         }
